3e07d6c824d7e1c0bc4cd6779677633fae9e4975
Angel Perez <164534035+Angel-Perez-1086@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('hola mundo');
+}
+
+void escribeNombre(){
+  print('Angel Perez Barron');
+}
+
+void main(){
+    // Angel Perez Barron Mat: 22308051281086
+    // Llamar a la funcion saludar 
+    print('Angel Perez Barron Mat: 22308051281086');
+    print('Llamando a la funcion saludar');
+    saludar();
+    print('Llamar a la funcion escribeNombre');
+    escribeNombre();
+}
